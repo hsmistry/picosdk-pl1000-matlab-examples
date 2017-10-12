95ed6da25a0615cf93b4f9d4eb0e1850fdabbd7a
hsmistry <hsmistry@users.noreply.github.com>
2017-10-12 13:35:18 +0100

    Add functions for data conversion

---
+++ pl1000adc2mv.m
@@ -0,0 +1,17 @@
+function millivolts = pl1000adc2mv(values, maxValue)
+%PL1000ADC2MV Converts milliVolt value to ADC count for PicoLog 1000 series
+%   values - raw ADC value(s)
+%   maxValue - maximum ADC count of the device
+%
+% See also pl1000mv2adc.
+%
+% Copyright © 2012-2017 Pico Technology Ltd. See LICENSE file for terms.
+
+    % Validate input parameters.
+    validateattributes(values, {'numeric'}, {'real', 'finite', 'nonnegative', 'nonnan'});
+    validateattributes(maxValue, {'numeric'}, {'scalar', 'integer', 'positive'});
+    
+    % Convert raw data.
+    millivolts = (double(values) * 2500 / maxValue);
+
+end
---
+++ pl1000mv2adc.m
@@ -0,0 +1,17 @@
+function adcCounts = pl1000mv2adc(values, maxValue)
+%PL1000ADC2MV Converts millivolt value to ADC count for PicoLog 1000 series
+%   values - value(s) to be converted (millivolts)
+%   maxValue - maximum ADC count of the device
+%
+% See also pl1000adc2mv.
+%
+% Copyright © 2012-2017 Pico Technology Ltd. See LICENSE file for terms.
+
+    % Validate input parameters.
+    validateattributes(values, {'numeric'}, {'real', 'finite', 'nonnegative', 'nonnan'});
+    validateattributes(maxValue, {'numeric'}, {'scalar', 'integer', 'positive'});
+    
+    % Convert data.
+    adcCounts = (values * double(maxValue)) / 2500;
+
+end
